146666d35829a6c8877f391b8ce64476e9c9f732
Achal Gupta <gupta.achal02@gmail.com>
2024-09-06 06:50:04 +0000

    Added test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,22 @@
+import pytest
+from calculator import Calculator
+
+def test_addition():
+    calc = Calculator()
+    assert calc.execute('add', 10, 5) == 15
+
+def test_subtraction():
+    calc = Calculator()
+    assert calc.execute('subtract', 10, 5) == 5
+
+def test_multiplication():
+    calc = Calculator()
+    assert calc.execute('multiply', 10, 5) == 50
+
+def test_division():
+    calc = Calculator()
+    assert calc.execute('divide', 10, 5) == 2.0
+
+def test_division_by_zero():
+    calc = Calculator()
+    assert calc.execute('divide', 10, 0) == "Error! Division by zero."
